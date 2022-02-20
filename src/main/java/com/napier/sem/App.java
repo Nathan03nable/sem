0a2dd69b655d28fab9f3f68729a643c2e2bd7520
Elisabeth Haubold <ejhaubold@gmailcom>
2022-02-20 23:02:58 +0000

    methods using database removed from App.java

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,99 +4,24 @@ import java.sql.*;
 
 public class App
 {
-    static final int RETRIES = 10;
-
     public static void main(String[] args)
     {
         App a = new App();
 
-        a.connect();
+        //a.connect();
 
-        CityDto cityDto = a.getACity(a.connection, 2);
+        //CityDto cityDto = a.getACity(a.connection, 2);
 
-        System.out.println(cityDto.toString());
+        //System.out.println(cityDto.toString());
 
-        a.disconnect();
+        //a.disconnect();
     }
 
     private CityDto getACity(Connection con, int id){
         City city = new City();
         return city.getCityById(con, id);
     }
-    /**
-     * Connection to MySQL database.
-     */
-    private Connection connection = null;
 
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect()
-    {
-        checkForSqlDriver();
-
-        for (int i = 0; i < RETRIES; ++i)
-        {
-            if (tryToConnect(i)){
-                break;
-            }
-        }
-    }
-
-    private void checkForSqlDriver() {
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
-    }
-
-    private boolean tryToConnect(int i) {
-        System.out.println("Connecting to database...");
-        try
-        {
-            Thread.sleep(30000);
-            // Change url to "jdbc:mysql://db:3306/Citys?useSSL=false" to run on docker
-            // Change url to "jdbc:mysql://localhost:33060/Citys?useSSL=false" to run locally
-            connection = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-            System.out.println("Successfully connected");
-            return true;
-        }
-        catch (SQLException sqle)
-        {
-            System.out.println("Failed to connect to database attempt " + i);
-            System.out.println(sqle.getMessage());
-        }
-        catch (InterruptedException ie)
-        {
-            System.out.println("Thread interrupted? Should not happen.");
-        }
-        return false;
-    }
-
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect()
-    {
-        if (connection != null)
-        {
-            try
-            {
-                // Close connection
-                connection.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
-    }
 
 
 }
