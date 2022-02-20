56950912ead1006c6f0ca5ed5dfffc733b596f15
Elisabeth Haubold <ejhaubold@gmailcom>
2022-02-20 23:24:25 +0000

    executeSQLStatement() method added to databaseConnection interface

--- src/main/java/com/napier/sem/databaseConnection.java
+++ src/main/java/com/napier/sem/databaseConnection.java
@@ -1,4 +1,9 @@
 package com.napier.sem;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public interface databaseConnection {
+
+    ResultSet executeSQLStatement(String sqlStatement) throws SQLException;
 }
