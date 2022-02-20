c177702ca63a51fac758995607f32aba097e1533
Elisabeth Haubold <ejhaubold@gmailcom>
2022-02-20 23:34:39 +0000

    SQL Exception removed from executeSQLStatement in databaseConnection interface

--- src/main/java/com/napier/sem/databaseConnection.java
+++ src/main/java/com/napier/sem/databaseConnection.java
@@ -1,9 +1,8 @@
 package com.napier.sem;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public interface databaseConnection {
 
-    ResultSet executeSQLStatement(String sqlStatement) throws SQLException;
+    ResultSet executeSQLStatement(String sqlStatement);
 }
