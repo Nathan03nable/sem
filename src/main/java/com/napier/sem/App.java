ae275b672eac345998216c4c018d99903830c695
Elisabeth Haubold <ejhaubold@gmailcom>
2022-02-21 00:41:20 +0000

    App.java modified to test database connection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,9 +4,21 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws SQLException {
         App a = new App();
+        databaseConnectionImpl db = new databaseConnectionImpl();
+        ResultSet rset = db.executeSQLStatement("select * from city where id='6'");
+
+        if(rset.next())
+        {
+            System.out.println("resultSet has next");
+            String name = rset.getString("Name");
+            System.out.println(name);
+        } else {
+            System.out.println("empty result");
+        }
+
+
 
         //a.connect();
 
