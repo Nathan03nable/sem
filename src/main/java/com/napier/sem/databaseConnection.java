f2e80c9744d82b975e215564e8ad2dfec85ba190
Elisabeth Haubold <ejhaubold@gmailcom>
2022-02-21 00:45:56 +0000

    Todo added in databaseConnection interface to change return type

--- src/main/java/com/napier/sem/databaseConnection.java
+++ src/main/java/com/napier/sem/databaseConnection.java
@@ -4,5 +4,6 @@ import java.sql.ResultSet;
 
 public interface databaseConnection {
 
+    //TODO: don't return a ResultSet, return a map(?) instead
     ResultSet executeSQLStatement(String sqlStatement);
 }
