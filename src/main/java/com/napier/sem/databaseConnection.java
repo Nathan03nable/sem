98933247a9dc7974b74d6e2c1770d52d69dc125b
Elisabeth Haubold <ejhaubold@gmail.com>
2022-02-21 11:38:00 +0000

    executeSQLStatement() changed to return a List

--- src/main/java/com/napier/sem/databaseConnection.java
+++ src/main/java/com/napier/sem/databaseConnection.java
@@ -1,9 +1,11 @@
 package com.napier.sem;
 
 import java.sql.ResultSet;
+import java.util.List;
+import java.util.Map;
 
 public interface databaseConnection {
 
     //TODO: don't return a ResultSet, return a map(?) instead
-    ResultSet executeSQLStatement(String sqlStatement);
+    List<Map<String, Object>> executeSQLStatement(String sqlStatement);
 }
--- src/main/java/com/napier/sem/databaseConnectionImpl.java
+++ src/main/java/com/napier/sem/databaseConnectionImpl.java
@@ -1,6 +1,10 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class databaseConnectionImpl implements databaseConnection{
 
@@ -12,24 +16,38 @@ public class databaseConnectionImpl implements databaseConnection{
     private Connection connection = null;
 
     /**
-     * Accepts an SQL statement, executes it and returns the result from the database
+     * Accepts an SQL statement, executes it and returns the result of the query
      * @param request a string in the form of an SQL statement
-     * @return ResultSet from the database. Returns null if there was an error.
+     * @return List of results from the database. Returns null if there was an error.
      */
     @Override
-    public ResultSet executeSQLStatement(String request) {
-        System.out.println(request);
+    public List<Map<String, Object>> executeSQLStatement(String request) {
         ResultSet result = null;
+        List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+        Map<String, Object> row = null;
+
         this.connect();
         try {
             Statement stmt = connection.createStatement();
             result = stmt.executeQuery(request);
+
+            ResultSetMetaData metaData = result.getMetaData();
+            Integer columnCount = metaData.getColumnCount();
+
+            while (result.next()) {
+                row = new HashMap<String, Object>();
+                for (int i = 1; i <= columnCount; i++) {
+                    row.put(metaData.getColumnName(i), result.getObject(i));
+                }
+                resultList.add(row);
+            }
         } catch (SQLException e){
             System.out.println("Failed to execute SQL statement '" + request +"'");
             System.out.println(e.getMessage());
         }
-        // this.disconnect();
-        return result;
+
+        this.disconnect();
+        return resultList;
     }
 
     /**
