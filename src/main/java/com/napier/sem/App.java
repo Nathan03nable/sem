b9bfad2b81fc23d3f79a11298ba438cf4fce0d09
Elisabeth Haubold <ejhaubold@gmail.com>
2022-02-21 13:03:10 +0000

    App.java modified to test ReportLanguage class with database injection

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,13 +1,11 @@
 package com.napier.sem;
 
 import java.sql.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 public class App
 {
     public static void main(String[] args) throws SQLException {
+        /*
         App a = new App();
         databaseConnectionImpl db = new databaseConnectionImpl();
         List<Map<String, Object>> result = db.executeSQLStatement("select * from city where name like 'A%' order by CountryCode asc");
@@ -15,22 +13,11 @@ public class App
         for (int i = 0; i < result.size(); i++){
             System.out.println(result.get(i).toString());
         }
+        */
 
+        DatabaseConnection db = DatabaseConnectionImpl.getInstance();
+        ReportLanguage languageReport = new ReportLanguage(db);
+        System.out.println(languageReport.generateReport("some string that doesn't do anything yet"));
 
-        //a.connect();
-
-        //CityDto cityDto = a.getACity(a.connection, 2);
-
-        //System.out.println(cityDto.toString());
-
-        //a.disconnect();
     }
-
-    private CityDto getACity(Connection con, int id){
-        City city = new City();
-        return city.getCityById(con, id);
-    }
-
-
-
 }
