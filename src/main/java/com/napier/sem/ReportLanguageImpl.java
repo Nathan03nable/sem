c3e683666c78c7bb1f3eb789c7729429655e677a
Elisabeth Haubold <ejhaubold@gmailcom>
2022-02-28 14:17:38 +0000

    comment removed in ReportLanguageImpl

--- src/main/java/com/napier/sem/ReportLanguageImpl.java
+++ src/main/java/com/napier/sem/ReportLanguageImpl.java
@@ -13,12 +13,5 @@ public class ReportLanguageImpl implements ReportGenerator{
 
         return dbConnection.executeSQLStatement(sqlStatement);
 
-        /*
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < result.size(); i++){
-            sb.append(result.get(i).toString()+ "\n");
-        }
-        return sb.toString();
-         */
     }
 }
