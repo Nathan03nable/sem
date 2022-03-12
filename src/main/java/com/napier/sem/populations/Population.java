853010f4cde92c4c33419e2e551b2d4cfb6f7d03
NathanPr033 <npringle1233@btinternet.com>
2022-03-12 16:03:24 +0000

    Add SQL statement for population of a continent

--- src/main/java/com/napier/sem/populations/Population.java
+++ src/main/java/com/napier/sem/populations/Population.java
@@ -13,12 +13,25 @@ public class Population {
 
   public void worldPopulation(){
     IDatabaseConnection databaseConnection = DatabaseConnectionImpl.getInstance();
-    String stmt = "SELECT SUM(DISTINCT(country.population)) AS 'World Population ',"
-        + "sum(city.population) AS 'Cities Population ',"
-        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population% ',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population ',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population% '"
+    String stmt = "SELECT SUM(DISTINCT(country.population)) AS 'World Population',"
+        + "sum(city.population) AS 'Cities Population',"
+        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
         + "FROM country JOIN city ON CountryCode = Code;";
+    ReportLanguageImpl languageReport = new ReportLanguageImpl(databaseConnection);
+    System.out.println(languageReport.generateReport(stmt));
+  }
+
+  public void continentPopulation(){
+    IDatabaseConnection databaseConnection = DatabaseConnectionImpl.getInstance();
+    String stmt = "SELECT SUM(DISTINCT(country.population)) AS 'World Population',"
+            + "sum(city.population) AS 'Cities Population',"
+            + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
+            + "FROM country JOIN city ON CountryCode = Code "
+            + "WHERE country.Continent LIKE 'Europe';";
     ReportLanguageImpl languageReport = new ReportLanguageImpl(databaseConnection);
     System.out.println(languageReport.generateReport(stmt));
   }
