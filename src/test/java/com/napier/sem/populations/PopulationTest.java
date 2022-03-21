468dfd5ceff86752a55f12b454636bc840132fe3
NathanPr03 <84146628+NathanPr03@users.noreply.github.com>
2022-03-21 21:40:09 +0000

    Add unit test for city class (#52)
    Co-authored-by: NathanPr033 <npringle1233@btinternet.com>

--- src/test/java/com/napier/sem/populations/PopulationTest.java
+++ src/test/java/com/napier/sem/populations/PopulationTest.java
@@ -1,7 +1,8 @@
-package com.napier.sem;
+package com.napier.sem.populations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.napier.sem.SqlManager;
 import com.napier.sem.populations.Population;
 import org.junit.jupiter.api.*;
 import org.mockito.Mock;
@@ -31,7 +32,8 @@ public class PopulationTest {
         + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
         + "FROM country JOIN city ON CountryCode = Code;";
 
-    String expected = "{World Population=6078547450, Cities Population=1429559884, Cities Population%=23.5181, Rural Population=4648987566, Rural Population%=76.4819}";
+    String expected = "String returned";
+
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
     String result = subject.worldPopulation();
@@ -51,7 +53,7 @@ public class PopulationTest {
         + "WHERE country.Continent LIKE 'Europe' "
         + "Group BY country.Continent;";
 
-    String expected = "{Continent=Europe, Continent Population=730074600, Cities Population=241942813, Cities Population%=33.1395, Rural Population=488131787, Rural Population%=66.8605}";
+    String expected = "String returned";
 
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
@@ -72,7 +74,7 @@ public class PopulationTest {
         + "WHERE country.region LIKE 'Caribbean' "
         + "Group BY country.region;";
 
-    String expected = "{Region=Caribbean, Region Population=38102000, Cities Population=11067550, Cities Population%=29.0472, Rural Population=27034450, Rural Population%=70.9528}";
+    String expected = "String returned";
 
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
@@ -81,7 +83,7 @@ public class PopulationTest {
   }
 
   @Test
-  void countryPopulation()
+  void countryPopulationTest()
   {
     String stmt = "SELECT country.name, "
         + "SUM(DISTINCT(country.population)) AS 'Region Population',"
@@ -93,7 +95,7 @@ public class PopulationTest {
         + "WHERE country.name LIKE 'United Kingdom' "
         + "Group BY country.code;";
 
-    String expected = "{Name=United Kingdom, Region Population=59623400, Cities Population=22436673, Cities Population%=37.6307, Rural Population=37186727, Rural Population%=62.3693}";
+    String expected = "String returned";
 
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
@@ -110,7 +112,7 @@ public class PopulationTest {
         + "WHERE city.district LIKE 'Michigan' "
         + "Group BY city.district;";
 
-    String expected = "{District=Michigan, District Population=1870428}";
+    String expected = "String returned";
 
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
@@ -127,8 +129,7 @@ public class PopulationTest {
         + "WHERE city.name LIKE 'Edinburgh' "
         + "Group BY city.name;";
 
-
-    String expected = "{Name=Edinburgh, City Population=450180}";
+    String expected = "String returned";
 
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
