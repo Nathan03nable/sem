989c888bbe6ceef96bb4c0cc89b6d5868ff0f6c5
Nathan <nathan.pringle@n-able.com>
2022-03-22 15:37:32 +0000

    Add test coverage

--- src/test/java/com/napier/sem/populations/PopulationTest.java
+++ src/test/java/com/napier/sem/populations/PopulationTest.java
@@ -16,11 +16,11 @@ public class PopulationTest {
   @Mock
   private SqlManager sqlManager;
 
-  private static final String CONTINENT = "Africa";
-  private static final String REGION = "Caribbean";
-  private static final String COUNTRY = "Ukraine";
-  private static final String DISTRICT = "Kabol";
-  private static final String CITY = "Edinburgh";
+  private static final String CONTINENT = "'Europe'";
+  private static final String REGION = "'Caribbean'";
+  private static final String COUNTRY = "'Ukraine'";
+  private static final String DISTRICT = "'Kabol'";
+  private static final String CITY = "'Edinburgh'";
 
   @BeforeEach
   public void init(){
@@ -49,15 +49,14 @@ public class PopulationTest {
   @Test
   void continentPopulationTest()
   {
-    String stmt = "SELECT country.Continent,"
-        + "SUM(DISTINCT(country.population)) AS 'Continent Population',"
-        + "sum(city.population) AS 'Cities Population',"
-        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
-        + "FROM country JOIN city ON CountryCode = Code "
-        + "WHERE country.Continent LIKE 'Europe' "
-        + "Group BY country.Continent;";
+    String stmt = String.format(
+        "SELECT country.Continent,SUM(DISTINCT(country.population)) AS 'Continent Population',"
+            + "sum(city.population) AS 'Cities Population', "
+            + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%%', "
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population', "
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%%' "
+            + "FROM country JOIN city ON CountryCode = Code "
+            + "WHERE country.Continent LIKE %s Group BY country.Continent;", CONTINENT);
 
     String expected = "String returned";
 
@@ -70,15 +69,15 @@ public class PopulationTest {
   @Test
   void regionPopulationTest()
   {
-    String stmt = "SELECT country.region, "
-        + "SUM(DISTINCT(country.population)) AS 'Region Population',"
-        + "sum(city.population) AS 'Cities Population',"
-        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
-        + "FROM country JOIN city ON CountryCode = Code "
-        + "WHERE country.region LIKE 'Caribbean' "
-        + "Group BY country.region;";
+    String stmt = String.format(
+        "SELECT country.region, "
+            + "SUM(DISTINCT(country.population)) AS 'Region Population', "
+            + "sum(city.population) AS 'Cities Population', "
+            + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%%', "
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population', "
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%%' "
+            + "FROM country JOIN city ON CountryCode = Code "
+            + "WHERE country.region LIKE %s Group BY country.region;", REGION);
 
     String expected = "String returned";
 
@@ -91,15 +90,16 @@ public class PopulationTest {
   @Test
   void countryPopulationTest()
   {
-    String stmt = "SELECT country.name, "
-        + "SUM(DISTINCT(country.population)) AS 'Region Population',"
-        + "sum(city.population) AS 'Cities Population',"
-        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
-        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
-        + "FROM country JOIN city ON CountryCode = Code "
-        + "WHERE country.name LIKE 'United Kingdom' "
-        + "Group BY country.code;";
+    String stmt = String.format(
+        "SELECT country.name, "
+            + "SUM(DISTINCT(country.population)) AS 'Region Population',"
+            + "sum(city.population) AS 'Cities Population',"
+            + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%%',"
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
+            + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%%'"
+            + "FROM country JOIN city ON CountryCode = Code "
+            + "WHERE country.name LIKE %s"
+            + "Group BY country.code;", COUNTRY);
 
     String expected = "String returned";
 
@@ -112,11 +112,12 @@ public class PopulationTest {
   @Test
   void districtPopulationTest()
   {
-    String stmt = "SELECT city.district, "
-        + "SUM(DISTINCT(city.population)) AS 'District Population' "
-        + "FROM city "
-        + "WHERE city.district LIKE 'Michigan' "
-        + "Group BY city.district;";
+    String stmt = String.format(
+        "SELECT city.district, "
+            + "SUM(DISTINCT(city.population)) AS 'District Population' "
+            + "FROM city "
+            + "WHERE city.district LIKE %s "
+            + "Group BY city.district;", DISTRICT);
 
     String expected = "String returned";
 
@@ -129,11 +130,12 @@ public class PopulationTest {
   @Test
   void cityPopulationTest()
   {
-    String stmt = "SELECT city.name, "
-        + "SUM(DISTINCT(city.population)) AS 'City Population' "
-        + "FROM city "
-        + "WHERE city.name LIKE 'Edinburgh' "
-        + "Group BY city.name;";
+    String stmt = String.format(
+        "SELECT city.name, "
+            + "SUM(DISTINCT(city.population)) AS 'City Population' "
+            + "FROM city "
+            + "WHERE city.name LIKE %s "
+            + "Group BY city.name;", CITY);
 
     String expected = "String returned";
 
@@ -142,4 +144,65 @@ public class PopulationTest {
     String result = subject.cityPopulation(CITY);
     assertEquals(sqlManager.executeStatement(stmt), result);
   }
+
+  @Test
+  void everyContinentPopulationTest()
+  {
+    String stmt = "SELECT country.Continent,"
+        + "SUM(DISTINCT(country.population)) AS 'Continent Population',"
+        + "sum(city.population) AS 'Cities Population',"
+        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
+        + "FROM country JOIN city ON CountryCode = Code "
+        + "Group BY country.Continent;";
+
+    String expected = "String returned";
+
+    Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
+
+    String result = subject.everyContinentPopulation();
+    assertEquals(sqlManager.executeStatement(stmt), result);
+  }
+
+  @Test
+  void everyRegionPopulationTest()
+  {
+    String stmt = "SELECT country.region, "
+        + "SUM(DISTINCT(country.population)) AS 'Region Population',"
+        + "sum(city.population) AS 'Cities Population',"
+        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
+        + "FROM country JOIN city ON CountryCode = Code "
+        + "Group BY country.region;";
+
+    String expected = "String returned";
+
+    Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
+
+    String result = subject.everyRegionPopulation();
+    assertEquals(sqlManager.executeStatement(stmt), result);
+  }
+
+  @Test
+  void everyCountryPopulationTest()
+  {
+    String stmt = "SELECT country.name, "
+        + "SUM(DISTINCT(country.population)) AS 'Region Population',"
+        + "sum(city.population) AS 'Cities Population',"
+        + "(sum(city.population) / SUM(DISTINCT(country.population))) * 100 AS 'Cities Population%',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) AS 'Rural Population',"
+        + "(SUM(DISTINCT(country.population)) - sum(city.population)) / SUM(DISTINCT(country.population)) * 100 AS 'Rural Population%'"
+        + "FROM country JOIN city ON CountryCode = Code "
+        + "Group BY country.code;";
+
+    String expected = "String returned";
+
+    Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
+
+    String result = subject.everyCountryPopulation();
+    assertEquals(sqlManager.executeStatement(stmt), result);
+  }
+
 }
