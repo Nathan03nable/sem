c5f2b7252ae5f6b16a19db7b3fdd788893680f0c
Nathan <nathan.pringle@n-able.com>
2022-03-22 10:32:37 +0000

    Increase test coverage

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -1,5 +1,10 @@
 package com.napier.sem;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.sql.SQLSyntaxErrorException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +12,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AppIntegrationTest
 {
-    static App app;
+    static App subject;
 
     @BeforeAll
     static void init()
     {
-        app = new App();
+        subject = new App();
     }
 
     @Test
@@ -22,6 +27,16 @@ public class AppIntegrationTest
         String statement = "Select Name from city where id='1';";
         String response = databaseConnection.executeSQLStatement(statement);
         assertEquals("{Name=Kabul}", response);
+    }
+
+    @Test
+    void testExecuteInvalidSqlStatementShouldThrowSQLException(){
+        String location = "localhost:33060";
+        IDatabaseConnection databaseConnection = DatabaseConnectionImpl.getInstance(location);
+        String invalidStatement = "Select;";
+
+        String result = databaseConnection.executeSQLStatement(invalidStatement);
+        assertEquals("", result);
     }
 
 }
