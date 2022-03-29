7ed0278c0c2d358e7ba90610638a144f35b49c1b
Lewis Bogle <bogfly2000@hotmail.com>
2022-03-29 11:52:25 +0100

    Add Country tests

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,11 @@
 package com.napier.sem;
 
 import com.napier.sem.population.Population;
+import com.napier.sem.countries.Country;
 
 public class App
 {
-    //static final int limit = 5;
+    static final String limit = "5";
     private static final String CONTINENT = "'Europe'";
     private static final String REGION = "'Caribbean'";
     private static final String COUNTRY = "'Ukraine'";
@@ -24,9 +25,18 @@ public class App
 
         Population population = new Population(sqlManager);
 
+        Country country = new Country(sqlManager);
+
         System.out.println(population.districtPopulation(DISTRICT));
         System.out.println(population.cityPopulation(CITY));
         System.out.println(population.countryPopulation(COUNTRY));
+
+        System.out.println(country.worldCountries());
+        System.out.println(country.continentCountries());
+        System.out.println(country.regionCountries());
+        System.out.println(country.topNPopulatedCountries(limit));
+        System.out.println(country.topNPopulatedContinentCountries(limit));
+        System.out.println(country.topNPopulatedRegionalCountries(limit));
 
         databaseConnection.disconnect();
     }
