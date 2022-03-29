f9fd72fd7a127fa3fd81054430de0998e03028d1
Nathan <nathan.pringle@n-able.com>
2022-03-29 19:16:45 +0100

    Fix test

--- src/test/java/com/napier/sem/unit_tests/languages/LanguageTest.java
+++ src/test/java/com/napier/sem/unit_tests/languages/LanguageTest.java
@@ -13,7 +13,7 @@ import org.mockito.MockitoAnnotations;
 
 public class LanguageTest {
 
-  private static final String WORLD_POPULATIOn = "2";
+  private static final String WORLD_POPULATION = "2";
   private Language subject;
 
   @Mock
@@ -28,28 +28,28 @@ public class LanguageTest {
   @Test
   void languageReportsTest()
   {
-    String stmt = String.format("SELECT"
-        + "CASE"
-        + "WHEN countrylanguage.Language like 'Chinese' THEN SUM(country.population)"
-        + "WHEN countrylanguage.Language like 'English' THEN SUM(country.population)"
-        + "WHEN countrylanguage.Language like 'Hindi' THEN SUM(country.population)"
-        + "WHEN countrylanguage.Language like 'Spanish' THEN SUM(country.population)"
-        + "WHEN countrylanguage.Language like 'Arabic' THEN SUM(country.population) END AS Population,"
-        + "SUM(population) / %s * 100 AS '%% of world population'"
-        + "FROM country JOIN countrylanguage on country.Code = countrylanguage.CountryCode"
-        + "WHERE countrylanguage.language like 'Chinese'"
-        + "   OR countrylanguage.language like 'English'"
-        + "   OR countrylanguage.language like 'Hindi'"
-        + "   OR countrylanguage.language like 'Spanish'"
-        + "   OR countrylanguage.language like 'Arabic'"
-        + "GROUP BY countrylanguage.Language"
-        + "ORDER BY SUM(country.population) DESC;", WORLD_POPULATIOn);
+    String stmt = String.format("SELECT countrylanguage.language, "
+        + "CASE "
+        + "WHEN countrylanguage.Language like 'Chinese' THEN SUM(country.population) "
+        + "WHEN countrylanguage.Language like 'English' THEN SUM(country.population) "
+        + "WHEN countrylanguage.Language like 'Hindi' THEN SUM(country.population) "
+        + "WHEN countrylanguage.Language like 'Spanish' THEN SUM(country.population) "
+        + "WHEN countrylanguage.Language like 'Arabic' THEN SUM(country.population) END AS Population, "
+        + "SUM(population) / %s * 100 AS '%% of world population' "
+        + "FROM country JOIN countrylanguage on country.Code = countrylanguage.CountryCode "
+        + "WHERE countrylanguage.language like 'Chinese' "
+        + "   OR countrylanguage.language like 'English' "
+        + "   OR countrylanguage.language like 'Hindi' "
+        + "   OR countrylanguage.language like 'Spanish' "
+        + "   OR countrylanguage.language like 'Arabic' "
+        + "GROUP BY countrylanguage.Language "
+        + "ORDER BY SUM(country.population) DESC; ", WORLD_POPULATION);
 
     String expected = "String returned";
 
     Mockito.when(sqlManager.executeStatement(stmt)).thenReturn(expected);
 
-    String result = subject.languagesReport(WORLD_POPULATIOn);
+    String result = subject.languagesReport(WORLD_POPULATION);
     assertEquals(sqlManager.executeStatement(stmt), result, "Should return expected string");
   }
 }
