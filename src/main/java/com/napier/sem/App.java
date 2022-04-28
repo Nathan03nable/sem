345cf28a2a354b29026ee081bb86e26d62c70976
Nathan03nable <90626193+Nathan03nable@users.noreply.github.com>
2022-04-28 16:27:17 +0100

    Add first 8 reports (#83)

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,18 +5,19 @@ import com.napier.sem.languages.Language;
 import com.napier.sem.population.Population;
 import com.napier.sem.countries.Country;
 import com.napier.sem.cities.City;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class App
 {
     static final String limit = "5";
-    static final String continent = "Africa";
+    static final String continent = "'Oceania'";
     static final String countryName = "'Spain'";
     static final String district = "'Fujian'";
-    static final String region = "Middle East";
+    static final String region = "'Caribbean'";
     static final String cityName = "'London'";
-
-    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
     public static void main(String[] args) {
         String location;
@@ -35,45 +36,17 @@ public class App
         CapitalCity capitalCity = new CapitalCity(sqlManager);
         Language language = new Language(sqlManager);
 
-        LOGGER.info(population.worldPopulation());
-        LOGGER.info(population.cityPopulation(cityName));
-        LOGGER.info(population.countryPopulation(countryName));
-        LOGGER.info(population.districtPopulation(region));
-        LOGGER.info(population.continentPopulation(continent));
+        System.out.println(country.worldCountries());
+        System.out.println(country.continentCountries(continent));
+        System.out.println(country.regionCountries(region));
+        System.out.println(country.topNPopulatedCountries(limit));
+        System.out.println(country.topNPopulatedContinentCountries(continent, limit));
+        System.out.println(country.topNPopulatedRegionalCountries(region, limit));
+        System.out.println(city.worldCities());
+        System.out.println(city.continentCities(continent));
 
-        LOGGER.info(population.everyContinentPopulation());
-        LOGGER.info(population.everyCountryPopulation());
-        LOGGER.info(population.everyRegionPopulation());
-        LOGGER.info(population.topNPopulatedCountriesInWorld(countryName, limit));
-        LOGGER.info(population.topNPopulatedCountriesInContinent(continent, limit));
-        LOGGER.info(population.topNPopulatedCountriesInRegion(region, limit));
-
-        LOGGER.info(country.continentCountries(continent));
-        LOGGER.info(country.regionCountries(region));
-        LOGGER.info(country.worldCountries());
-        LOGGER.info(country.topNPopulatedContinentCountries(continent, limit));
-        LOGGER.info(country.topNPopulatedCountries(limit));
-        LOGGER.info(country.topNPopulatedRegionalCountries(region, limit));
-
-        LOGGER.info(city.continentCities(continent));
-        LOGGER.info(city.countryCities(countryName));
-        LOGGER.info(city.districtCities(district));
-        LOGGER.info(city.regionCities(region));
-        LOGGER.info(city.topNPopulatedCitiesInARegion(region, limit));
-        LOGGER.info(city.topNPopulatedCitiesInAContinent(continent, limit));
-        LOGGER.info(city.topNPopulatedCitiesInACountry(countryName, limit));
-        LOGGER.info(city.topNPopulatedCitiesInADistrict(district, limit));
-        LOGGER.info(city.worldCities());
-
-        LOGGER.info(capitalCity.worldCapitalCities());
-        LOGGER.info(capitalCity.regionCapitalCities(region));
-        LOGGER.info(capitalCity.continentCapitalCities(region));
-        LOGGER.info(capitalCity.topNPopulatedCapitalCitiesInTheWorld(limit));
-        LOGGER.info(capitalCity.topNPopulatedCapitalCitiesInARegion(region, limit));
-        LOGGER.info(capitalCity.topNPopulatedCapitalCitiesInAContinent(continent, limit));
 
         String worldPop = population.extractWorldPopulation(population.worldPopulation());
-        LOGGER.info(language.languagesReport(worldPop));
 
         databaseConnection.disconnect();
     }
